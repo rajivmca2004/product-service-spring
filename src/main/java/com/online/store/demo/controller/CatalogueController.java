eecfab4e42c021e812e94edc8406a00070a92deb
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-08-13 11:29:29 +0530

    Adding Spring Cloud config files and code

--- src/main/java/com/online/store/demo/controller/CatalogueController.java
+++ src/main/java/com/online/store/demo/controller/CatalogueController.java
@@ -2,15 +2,15 @@ package com.online.store.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.online.store.demo.model.Catalogue;
 import com.online.store.demo.repository.CatalogueRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author rasrivastava
@@ -18,18 +18,24 @@ import org.slf4j.LoggerFactory;
  */
 @RestController
 public class CatalogueController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(CatalogueController.class);
 
 	@Autowired
 	private CatalogueRepository catalogueRepository;
 
-    
-    @GetMapping("/catalogue")
-    public Object fetchProducts1 ()
-    {
-        List<Catalogue> products = catalogueRepository.findAll();
-        logger.info("**Calling /catalogue ");
-        return products;
-    }
+	@GetMapping("/catalogue")
+	public Object fetchProducts1() {
+		List<Catalogue> products = catalogueRepository.findAll();
+		logger.info("**Calling /catalogue ");
+		return products;
+	}
+
+	@Value("${myenv.name:Config server issue}")
+	private String envName;
+
+	@GetMapping("/checkEnvName")
+	public String getMyEnv() {
+		return this.envName;
+	}
 }
